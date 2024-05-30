60ad3262b4dfd146ed7263e6ca8489fc029c88c6
Paola Andrea Moreno <136043704+IngSw-Moreno7@users.noreply.github.com>
2024-05-30 14:29:27 -0500

    CRUD complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import { Persons } from './components/Persons';
+import { useState } from 'react'; 
 
 function App() {
+  const [persons, setPersons] = useState([
+    {
+      id: 1,
+      name: 'Ninna Arroye',
+      role: 'Diseñadora UX/UI',
+      img: 'https://bootdey.com/img/Content/avatar/avatar3.png',
+    },
+    {
+      id: 2,
+      name: 'Alejandro Degli Sposti',
+      role: 'Arquitecto de Software',
+      img: 'https://bootdey.com/img/Content/avatar/avatar1.png',
+    },
+    {
+      id: 3,
+      name: 'Jeremmy Sigg',
+      role: 'Ingeniero de DevOps',
+      img: 'https://bootdey.com/img/Content/avatar/avatar2.png',
+    },
+  ]);
+
   return (
-    <div>
-      <Persons />
+    <div className='App'>
+      <div className='container'>
+        <Persons persons={persons} setPersons={setPersons} /> {/* Corregido */}
+      </div>
     </div>
   );
 }
--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -1,48 +1,107 @@
-import React from 'react';
-import { useState } from 'react';
-import { Person } from './Person'; // Assuming Person component is located in './Person'
+import React from 'react'
+import { Person } from './Person'
+import { useState } from 'react'
 
-export const Persons = () => {
-  const [persons] = useState([
-    {
-      id: 1,
-      name: 'Ninna Arroye',
-      role: 'Diseñadora UX/UI',
-      img: 'https://bootdey.com/img/Content/avatar/avatar3.png',
-    },
-    {
-      id: 2,
-      name: 'Alejandro Degli Sposti',
-      role: 'Arquitecto de Software',
-      img: 'https://bootdey.com/img/Content/avatar/avatar1.png',
-    },
-    {
-      id: 3,
-      name: 'Jeremy Mendez',
-      role: 'Ingeniero de DevOps',
-      img: 'https://bootdey.com/img/Content/avatar/avatar2.png',
-    },
-  ]);
+export const Persons = ({ persons, setPersons }) => {
+
+  const [editingId, setEditingID] = useState(null);
+  const [editedPerson, setEditedPerson] = useState({
+    name: '',
+    role: '',
+    img: ''
+  });
+  const [ isEditing, setIsEditing ] = useState(false);
+  const [personToDelete, setPersonToDelete] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value} = e.target;
+    setEditedPerson(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  }
+
+  const handleEdit = (id) => {
+    setEditingID(id);
+    setIsEditing(true);
+    const personToEdit = persons.find(person => person.id === id);
+    setEditedPerson({...personToEdit})
+  }
+  
+  const handleSave = (e) => {
+    setPersons(persons.map(person => person.id === editingId ? editedPerson : person ));
+    setEditingID(null);
+    setEditedPerson({ name: '', role: '', img: '' });
+    setIsEditing(false);
+  }
+
+  const handleDelete = (id) => {
+    setPersonToDelete(id);
+  }
+
+  const confirmDelete = (e) => {
+    setPersons(persons.filter(person => person.id !== personToDelete));
+    setPersonToDelete(null);
+  }
+
+  const cancelDelete = (e) => {
+    setPersonToDelete(null);
+  }
+
+  const handleCreate = (e) => {
+    setPersons([...persons, { id: persons.length + 1, ...editedPerson }]);
+    setEditedPerson({ name: '', role: '', img: ''});
+  }
 
   return (
     <div>
       <h2>IT Team</h2>
-        <div className='container d-flex justify-content-center'>
-          <div className="d-flex flex-row">
-            {persons.map(person => {
-              return (
-                <div>
-                  <Person
-                    id={person.id}
-                    name={person.name}
-                    role={person.role}
-                    img={person.img}
-                  />
-                </div>
-              );
-            })}
+      <div className='container'>
+        <div className='row d-flex flex-wrap row-cols-1 row-cols-md-2 row-cols-lg-3  '>
+          {persons.map((person) => {
+            return (
+              <div key={person.id}>
+                <Person
+                  id={person.id}
+                  name={person.name}
+                  role={person.role}
+                  img={person.img}
+                  handleEdit={() => handleEdit(person.id)}
+                  handleDelete={handleDelete}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      {/* Renderiza el formulario para editar o crear una persona */}
+      <div className='mt-4 row p-2'>
+          <h2>{isEditing ? 'Modificar Datos' : 'Datos de la Nueva Persona'}</h2>
+          <input type="text" name="name" value={editedPerson.name} onChange={handleChange} placeholder='Nombre' className='form-control mb-2'/>
+          <input type="text" name="role" value={editedPerson.role} onChange={handleChange} placeholder='Rol' className='form-control mb-2'/>
+          <input type="text" name="img" value={editedPerson.img} onChange={handleChange} placeholder='URL de la imagen' className='form-control mb-2'/>
+          <div className='mt-2'>
+            <button className='btn btn-primary' onClick={isEditing ? handleSave : handleCreate}>{isEditing ? 'Guardar' : 'Crear'}</button>
+          </div>
+      </div>
+      {/* Modal de confirmación de eliminación */}
+      <div id="deleteModal" className='modal fade' tabIndex="-1">
+        <div className='modal-dialog'>
+          <div className='modal-content'>
+            <div className='modal-header'>
+              <h4 className='modal-title'>Confirmar Eliminación</h4>
+              <button type="button" className='btn-close' data-bs-dismiss="modal" aria-label="Close" onClick={cancelDelete}></button>
+            </div>
+            <div className='modal-body'>
+              <p>¿Estás seguro de eliminar a {persons.find(person => person.id === personToDelete)?.name}</p>
+            </div>
+            <div className='modal-footer'>
+              <button type="button" className='btn btn-secondary' data-bs-dismiss="modal" onClick={cancelDelete}>Cancelar</button>
+              <button type="button" className='btn btn-danger' data-bs-dismiss="modal" onClick={confirmDelete}>Eliminar</button>
+            </div>
           </div>
         </div>
       </div>
-  );
-};
+    </div>
+  )
+}
